094adf4327ca63d2b7725598c6ef99d09a993ae0
Lenny Schwarz <lennyschwarz06@gmail.com>
2025-09-18 19:18:48 +0100

    Added the can_take method

--- king.py
+++ king.py
@@ -15,3 +15,11 @@ class King(Chess_Piece):
             return False
         
         return False
+
+    def can_take(self, board : list, x_destination : int, y_destination : int) -> bool:
+        if abs(self.x - x_destination) == 1 and abs(self.y - y_destination) == 1 and \
+              self.team != board[x_destination][y_destination].get_team():
+            return True
+        else:
+            return False
+

19b7a320df0ff1caf357341ee546e0cffb13e73c
Lenny Schwarz <lennyschwarz06@gmail.com>
2025-09-18 17:45:36 +0100

    The main chess piece class, for the others to inherit from

---
+++ chess_piece.py
@@ -0,0 +1,23 @@
+class Chess_Piece:
+    def __init__(self, x : int, y : int) -> None:
+        self.x = x
+        self.y = y
+        self.can_change = False
+        self.times_moved = 0
+    
+    def get_x(self):
+        return self.x
+    
+    def get_y(self):
+        return self.y
+    
+    def set_x(self, x):
+        if x >= 0 and x <= 7:
+            self.x = x
+    
+    def set_y(self, y):
+        if y >= 0 and y <= 7:
+            self.y = y
+    
+    def get_times_moved(self):
+        return self.times_moved

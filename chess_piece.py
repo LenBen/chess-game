404f03c851ffc76e2ca38d44adf349a59dc0b231
Lenny Schwarz <lennyschwarz06@gmail.com>
2025-09-20 18:23:32 +0100

    made  the setters return false if they are invalid

--- chess_piece.py
+++ chess_piece.py
@@ -6,22 +6,27 @@ class Chess_Piece:
         self.times_moved = 0
         self.team = team
     
-    def get_x(self):
+
+    def get_x(self) -> int:
         return self.x
     
-    def get_y(self):
+    def get_y(self) -> int:
         return self.y
     
-    def set_x(self, x):
+    def set_x(self, x) -> bool:
         if x >= 0 and x <= 7:
             self.x = x
+        else:
+            return False
     
-    def set_y(self, y):
+    def set_y(self, y) -> bool:
         if y >= 0 and y <= 7:
             self.y = y
+        else:
+            return False
     
-    def get_times_moved(self):
+    def get_times_moved(self) -> int:
         return self.times_moved
     
-    def get_team(self):
+    def get_team(self) -> int:
         return self.team

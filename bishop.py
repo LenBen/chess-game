5010f018ad20e5fc17c28b0e7f50e6db462f20fe
Lenny Schwarz <lennyschwarz06@gmail.com>
2025-09-26 16:01:01 +0100

    added material to the can_move method and created the can_take method

--- bishop.py
+++ bishop.py
@@ -5,4 +5,19 @@ class Bishop(Chess_Piece):
         super().__init__(x, y, team)
 
     def can_move(self, board : list, x_destination : int, y_destination : int) -> bool:
-        pass
+        if board[y_destination][x_destination] != "":
+            return False
+        
+        if abs(self.x - x_destination) != abs(self.y - y_destination):
+            return False
+        
+        return True
+    
+    def can_take(self, board : list, x_destination : int, y_destination : int) -> bool:
+        if abs(self.x - x_destination) != abs(self.y - y_destination):
+            return False
+        
+        if board[y_destination][x_destination].get_team() != self.team:
+            return True
+        
+        return False
